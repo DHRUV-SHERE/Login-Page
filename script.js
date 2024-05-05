be00a387c3ba497bacdfeb99dd89fe5ca1ddef47
Dhruv <sheredhruv@gmail.com>
2024-05-05 22:41:01 +0530

    Some Changes

--- script.js
+++ script.js
@@ -5,15 +5,59 @@ let signup1btn = document.querySelector("#signup1 button");
 let signup = document.querySelector("#signup");
 
 login1btn.addEventListener("click", function () {
-    gsap.to("#login", { opacity: 0 });
-    gsap.to("#signup", { opacity: 1 });
+    gsap.to("#login", { opacity: 0 ,duration :0.8});
+    gsap.to("#signup", { opacity: 1 ,duration :0.8});
     login.style.display = "none";
     signup.style.display = "flex";
 });
 
 signup1btn.addEventListener("click", function () {
-    gsap.to("#signup", { opacity: 0 });
-    gsap.to("#login", { opacity: 1 });
+    gsap.to("#signup", { opacity: 0 ,duration :0.8});
+    gsap.to("#login", { opacity: 1 ,duration :0.8});
     signup.style.display = "none";
     login.style.display = "flex";
 });
+
+
+let captcha;
+function generate() {
+
+    // Clear old input
+    document.getElementById("submit").value = "";
+
+    // Access the element to store
+    // the generated captcha
+    captcha = document.getElementById("image");
+    let uniquechar = "";
+
+    const randomchar =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    // Generate captcha for length of
+    // 5 with random character
+    for (let i = 1; i < 5; i++) {
+        uniquechar += randomchar.charAt(
+            Math.random() * randomchar.length)
+    }
+
+    // Store generated input
+    captcha.innerHTML = uniquechar;
+}
+
+function printmsg() {
+    const usr_input = document
+        .getElementById("submit").value;
+
+    // Check whether the input is equal
+    // to generated captcha or not
+    if (usr_input == captcha.innerHTML) {
+        let s = document.getElementById("key")
+            .innerHTML = "Matched";
+        generate();
+    }
+    else {
+        let s = document.getElementById("key")
+            .innerHTML = "not Matched";
+        generate();
+    }
+}
